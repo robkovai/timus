9d1cb15adb378f610a9f3da675f48e35c8392f45
robkovai <dedi4@mail.ru>
2019-09-30 00:42:35 +0300

    task_1409

---
+++ task_1409/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    unsigned short garry, larry;
+    cin >> garry >> larry;
+    cout << larry - 1 << " " << garry - 1 << endl;
+    return 0;
+}
