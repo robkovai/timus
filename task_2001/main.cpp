9f81c48bd2168ee965f66d0dbfa25d2d0730e92b
robkovai <dedi4@mail.ru>
2019-09-30 00:44:04 +0300

    task_2001

---
+++ task_2001/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    unsigned short a1, b1, a2, b2, a3, b3;
+    cin >> a1 >> b1 >> a2 >> b2 >> a3 >> b3;
+    cout << a1 - a3 << " " << b1 - b2 << endl;
+    return 0;
+}
